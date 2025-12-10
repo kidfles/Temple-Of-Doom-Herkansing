b689a04fc3807ada9f4e3f288fbf7d68e09371a9
kidfles <166709765+kidfles@users.noreply.github.com>
2025-12-10 19:05:18 +0100

    conveyor belt logic is now in 1 place

--- TempleOfDoom.Core/ConveyorBeltTile.cs
+++ TempleOfDoom.Core/ConveyorBeltTile.cs
@@ -8,21 +8,6 @@ namespace TempleOfDoom.Core
         public override void Interact(Player player)
         {
             base.Interact(player);
-            switch (Direction)
-            {
-                case Direction.NORTH:
-                    player.Y--;
-                    break;
-                case Direction.EAST:
-                    player.X++;
-                    break;
-                case Direction.SOUTH:
-                    player.Y++;
-                    break;
-                case Direction.WEST:
-                    player.X--;
-                    break;
-            }
         }
 
         public override bool IsWalkable(Player player)
