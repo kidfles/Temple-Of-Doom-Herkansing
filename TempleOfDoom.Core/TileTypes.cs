b663beaa5b226f0748ddced21738058d1e3506bd
kidfles <166709765+kidfles@users.noreply.github.com>
2025-12-10 18:56:18 +0100

    central tiletypes

---
+++ TempleOfDoom.Core/TileTypes.cs
@@ -0,0 +1,18 @@
+namespace TempleOfDoom.Core
+{
+    public static class TileTypes
+    {
+        public const string Floor = "floor";
+        public const string Wall = "wall";
+        public const string ConveyorBelt = "conveyor belt";
+        public const string SankaraStone = "sankara stone";
+        public const string Key = "key";
+        public const string BoobyTrap = "boobytrap";
+        public const string DisappearingBoobyTrap = "disappearing boobytrap";
+        
+        // Door Types
+        public const string Colored = "colored";
+        public const string Toggle = "toggle";
+        public const string ClosingGate = "closing gate";
+    }
+}
