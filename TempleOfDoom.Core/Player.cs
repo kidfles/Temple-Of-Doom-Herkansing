e0c3fc4639c5fb7ec3ee63f17888f3ed1067ae19
kidfles <166709765+kidfles@users.noreply.github.com>
2025-12-10 15:53:54 +0100

    added state to player to keep track of x,y and currentroomId

--- TempleOfDoom.Core/Player.cs
+++ TempleOfDoom.Core/Player.cs
@@ -4,6 +4,8 @@ namespace TempleOfDoom.Core
 {
     public class Player
     {
-       
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int CurrentRoomId { get; set; }
     }
 }
